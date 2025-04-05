102896b584ebb9af2317bcc094cc7b8bdf73f2e3
lebuckman <liambuckman0@gmail.com>
2025-04-04 17:40:20 -0700

    Complete exercise for arrays and loops section

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    const strArray = str.split("");
+    let reversedString = "";
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += strArray[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+    let celsius = ((tempInFahrenheit - 32) * 5) / 9;
+    return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+    let fahrenheit = tempInCelsius * (9 / 5) + 32;
+    return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
